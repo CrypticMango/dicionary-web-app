5e0937b9f50adfafc4d2775e03716c6d83f7e24b
CrypticMango <79679706+CrypticMango@users.noreply.github.com>
2021-09-30 18:42:42 -0600

    added more styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,8 +14,8 @@ let [keyword, setKeyword] = useState("");
     }
     return (
         <div className = "Dictionary"> <form onSubmit={search}>
-            <input type="search" placeholder="Search for a word" onChange={handleKeywordChange}/>
-            <input type="submit" value="Search"/>
+            <input type="search" placeholder="      Search for a word.." onChange={handleKeywordChange}/>
+            <input type="submit" value=" 🔍 "/>
         </form>
         </div>
 )
