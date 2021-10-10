d05fea3cc8e0416d72801095d37e2249e7d9821b
CrypticMango <79679706+CrypticMango@users.noreply.github.com>
2021-10-09 21:04:28 -0600

    moved position of "listen here"
    -changed font size of phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,11 +4,8 @@ import "./Phonetic.css"
 export default function Phonetics(props) {
     return (
         <div className="Phonetic">
-            <br />
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen Here 🔊</a>
-            <br />
-            <br />
             <p>/{props.phonetic.text}/</p>
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen Here 🔊</a>
         </div>
     )
 }
