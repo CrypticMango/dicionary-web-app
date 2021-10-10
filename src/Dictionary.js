8ca1098762022f83d46c47089faf365e4dc6089f
CrypticMango <79679706+CrypticMango@users.noreply.github.com>
2021-10-09 23:04:39 -0600

    added photo component
    -added styling to photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,31 +1,39 @@
 import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Photos from "./Photos";
 import Results from "./Results";
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
 let [results, setResults] =useState(null);
+let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
-        console.log(response.data[0].meanings[0].definitions[0].definition)
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        console.log(response);
+        setPhotos(response.data.photos);
     }
 
     function search(event) {
         event.preventDefault();
-
         // documentation: https://dictionaryapi.dev/
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
-    }
 
-   
+        let pexelsApiKey = "563492ad6f917000010000013b936fa5ae7c454d8ac663d0fd3d0870";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
+    }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
+    
     return (
         <div className = "Dictionary"> 
         
@@ -35,6 +43,7 @@ let [results, setResults] =useState(null);
             </form>
         
         <Results results={results}/>
+        <Photos photos={photos} />
         </div>
-)
-}
+);
+    }
